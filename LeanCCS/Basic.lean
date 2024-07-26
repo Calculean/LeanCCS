17c09cf49430d77af2e34a633f61ddd731f17f8d
BasharHamade <148087001+BasharHamade@users.noreply.github.com>
2024-07-26 20:51:31 +0200

    pre functions to be fixed

--- LeanCCS/Basic.lean
+++ LeanCCS/Basic.lean
@@ -85,3 +85,31 @@ def map_lts_statesList (lts : LTS) : List { s : State // s ∈ lts.states } :=
 
 def pre {lts : LTS} (s : { s : State // s ∈ lts.states }) (a : Action) : List { s : State // s ∈ lts.states } :=
   List.filter (fun st => (lts.transition st a).indexOf? s != none) <| map_lts_statesList lts
+
+def pre_A (lts : LTS) (s : State) (actions : List Action) : List State :=
+  List.filter (fun s' => actions.all (fun a => (post lts s' a).any (fun s'' => s'' == s))) lts.states
+
+def pre_S_a (lts : LTS) (states : List State) (a : Action) : List State :=
+  states.foldl (fun acc state => acc ++ pre lts state a) []
+
+def pre_S_A (lts : LTS) (states : List State) (actions : List Action) : List State :=
+  actions.foldl (fun acc action => acc ++ pre_S_a lts states action) []
+
+
+def pre_S_n (lts : LTS) (states : List State) (actions : List Action) (n : Nat) : List State :=
+  match n with
+  | 0 => states
+  | n' + 1 => pre_S_A lts (pre_S_n lts states actions n') actions
+
+
+def pre_star_aux (lts : LTS) (visited : List State) (to_visit : List State) : List State :=
+  match to_visit with
+  | [] => visited
+  | s::ss =>
+    if elem s  visited then
+      pre_star_aux lts visited ss
+    else
+      pre_star_aux lts (s::visited) ((concatLists (List.map (fun a => pre lts s a) lts.actions)) ++ ss)
+
+def pre_star (lts : LTS) (initial_states : List State) : List State :=
+  pre_star_aux lts [] initial_states
